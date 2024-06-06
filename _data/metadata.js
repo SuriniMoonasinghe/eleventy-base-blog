92612fc31dc94fc82782ec39c1cf7db323afc671
Surini Moonasinghe <surini@live.co.uk>
2024-06-06 14:00:05 +0100

    task completed
    changed meta-data+ removed yellow box

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	url: "https://surinimoonasinghe.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences as a developer in training.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Surini",
+		email: "surini@live.co.uk",
 		url: "https://example.com/about-me/"
 	}
 }
